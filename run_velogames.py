e6af6920aa040f6f451014acfd946f6b02aa1e15
Juraj <juraj.panek@gmail.com>
2025-03-26 18:06:06 +0100

    Brugge de Panne files

--- run_velogames.py
+++ run_velogames.py
@@ -10,6 +10,15 @@ sixies = {
     "team_url":"https://www.velogames.com/sixes-superclasico/2025/teamroster.php?tid={team_id}"
 }   
 
+bruge = {
+    "name":"Supersixies",
+    "league_id":61627774,
+    "stage":"BruggeDePann",
+    "stage_id":7,
+    "url":"https://www.velogames.com/sixes-superclasico/2025/leaguescores.php?league={league_id}&ga=13&st={stage_id}",
+    "team_url":"https://www.velogames.com/sixes-superclasico/2025/teamroster.php?tid={team_id}"
+}
+
 #catalunya
 catalunya = {
     "name":"Catalunya",
@@ -18,7 +27,7 @@ catalunya = {
     "team_url":"https://www.velogames.com/catalunya/2025/teamroster.php?tid={team_id}"
 }
 
-race = catalunya
+race = bruge
 
 teams = get_teams(race)
 save_team_text(teams, race)
