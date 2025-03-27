9017ce9465b349dbd57eaac7bb4c15daf72782a3
Juraj <juraj.panek@gmail.com>
2025-03-27 19:05:34 +0100

    Auto update from CRON run at 2025-03-27 19:05:34

--- run_velogames.py
+++ run_velogames.py
@@ -1,6 +1,6 @@
 from velogames import get_teams, save_team_text, save_team_html
 from organize import generate_directory_html, generate_directory
-import os 
+from datetime import datetime
 
 #supersixies:
 torino = {
@@ -45,6 +45,7 @@ e3 = {
     "league_id":61627774,
     "stage":"E3",
     "stage_id":8,
+    "race_date":"2025-03-28",
     "url":"https://www.velogames.com/sixes-superclasico/2025/leaguescores.php?league={league_id}&ga=13&st={stage_id}",
     "team_url":"https://www.velogames.com/sixes-superclasico/2025/teamroster.php?tid={team_id}&ga=13&st={stage_id}"
 }
@@ -54,16 +55,62 @@ gent = {
     "league_id":61627774,
     "stage":"Gent-Wevelgem",
     "stage_id":9,
+    "race_date":"2025-03-30",
     "url":"https://www.velogames.com/sixes-superclasico/2025/leaguescores.php?league={league_id}&ga=13&st={stage_id}",
     "team_url":"https://www.velogames.com/sixes-superclasico/2025/teamroster.php?tid={team_id}&ga=13&st={stage_id}"
 }
 
+dwars = {
+    "name":"Supersixies",
+    "league_id":61627774,
+    "stage":"Dwars-door-Vlaanderen",
+    "stage_id":10,
+    "race_date":"2025-04-04",
+    "url":"https://www.velogames.com/sixes-superclasico/2025/leaguescores.php?league={league_id}&ga=13&st={stage_id}",
+    "team_url":"https://www.velogames.com/sixes-superclasico/2025/teamroster.php?tid={team_id}&ga=13&st={stage_id}"
+}
+
+rvv = {
+    "name":"Supersixies",
+    "league_id":61627774,
+    "stage":"RVV",
+    "stage_id":11,
+    "race_date":"2025-04-06",
+    "url":"https://www.velogames.com/sixes-superclasico/2025/leaguescores.php?league={league_id}&ga=13&st={stage_id}",
+    "team_url":"https://www.velogames.com/sixes-superclasico/2025/teamroster.php?tid={team_id}&ga=13&st={stage_id}"
+}
+
+scheldeprijs = {
+    "name":"Supersixies",
+    "league_id":61627774,
+    "stage":"Scheldeprijs",
+    "stage_id":12,
+    "race_date":"2025-04-09",
+    "url":"https://www.velogames.com/sixes-superclasico/2025/leaguescores.php?league={league_id}&ga=13&st={stage_id}",
+    "team_url":"https://www.velogames.com/sixes-superclasico/2025/teamroster.php?tid={team_id}&ga=13&st={stage_id}"
+}
+
+roubaix = {
+    "name":"Supersixies",
+    "league_id":61627774,
+    "stage":"Roubaix",
+    "stage_id":13,
+    "race_date":"2025-04-13",
+    "url":"https://www.velogames.com/sixes-superclasico/2025/leaguescores.php?league={league_id}&ga=13&st={stage_id}",
+    "team_url":"https://www.velogames.com/sixes-superclasico/2025/teamroster.php?tid={team_id}&ga=13&st={stage_id}"
+}
+
+
+
 races = [torino, msr, bruge, e3, catalunya]
-races = [e3]
+races = [e3, gent, dwars, scheldeprijs, roubaix]
+
+today = datetime.today().date()
 
 for race in races:
-    teams = get_teams(race)
-    save_team_text(teams, race)
-    save_team_html(teams,race)
+    if datetime.strptime(race["race_date"], "%Y-%m-%d").date() == today:
+        teams = get_teams(race)
+        save_team_text(teams, race)
+        save_team_html(teams,race)
 
 generate_directory_html('race_data','index.html')
